3fc0373794c3fd74857a3442aa4086907a13b263
FerminVerdolini <ferminverdolini@gmail.com>
2024-04-16 22:30:23 +0200

    Se crea el principal segun el punto 3 del Tpy se hacen algunas correcciones en el pid

--- Practica5_SistemasDeControlDinamicoII/pid.adb
+++ Practica5_SistemasDeControlDinamicoII/pid.adb
@@ -1,19 +1,31 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
 package body PID is
 
 
     procedure Programar (el_Controlador: in out Controlador;
                             Kp, Ki, Kd:        Real) is 
     begin
-        Controlador.Kd := kd;
-        Controlador.Ki := Ki;
-        Controlador.Kp := kp;
+        el_Controlador.Kd := kd;
+        el_Controlador.Ki := Ki;
+        el_Controlador.Kp := kp;
     end Programar;
 
     procedure Controlar (con_el_Controlador: in out Controlador;
                                      R, C:        Entrada;
                                         U: out    Salida) is 
+    E : Real;
     begin
-        U := Controlador.Kp * ((R-C)+((1/Controlador.Ki)*Controlador.S_Anterior*0.2)+(Controlador.Kd*(((R-C)-Controlador.Error_Anterior)/(0.2))));
-    end Programar;
+        E := Real(R-C);
+
+        con_el_Controlador.S_Anterior := con_el_Controlador.S_Anterior + E; 
+    
+
+        U := Salida(con_el_Controlador.Kp * (E+
+                                            Real((Real(1)/con_el_Controlador.Ki)*con_el_Controlador.S_Anterior*Real(0.2))+
+                                            Real(con_el_Controlador.Kd*(((E)-con_el_Controlador.Error_Anterior)/Real(0.2)))));
+
+        con_el_Controlador.Error_Anterior := Real(E);
+    end Controlar;
 
 end PID;
---
+++ Practica5_SistemasDeControlDinamicoII/principal.adb
@@ -0,0 +1,59 @@
+with Sensor; with Calefactor; with PID;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Real_Time; use Ada.Real_Time;
+
+procedure Principal is
+    Ts: constant := 0.2;
+    T, Tref : Sensor.Temperaturas;
+    P : Calefactor.Potencias ;
+
+    Start_Time, Current_Time : Time;
+    Elapsed_Time : Time_Span;
+
+    Siguiente_Instante: Time;
+    Periodo: constant Time_Span := To_Time_Span(Ts);
+
+    package ES_Temp is new Ada.Text_IO.Float_IO(Sensor.Temperaturas);
+    package Control_Horno is new
+                   PID (Real => Float,
+                               Entrada => Sensor.Temperaturas,
+                                  Salida => Calefactor.Potencias);
+    
+    C : Control_Horno.Controlador;
+begin
+    Control_Horno.Programar(C,1.01164, 3.20038, 0.800095); --Paso las constantes que calcule en el punto 1
+    
+    Put("Ingrese temperatura de referencia:");New_Line;
+    ES_Temp.Get(Tref);    
+    
+    Siguiente_Instante := Clock;
+    Start_Time := Clock;
+
+    loop
+        delay until Siguiente_Instante;
+
+        Sensor.Leer(T);
+
+        Control_Horno.Controlar(C,Tref,T,P);
+
+        Calefactor.Escribir(P);
+
+        Put("Temperatura actual del horno : " & T'Image & "C");New_Line;
+
+        Current_Time := Clock;
+        Elapsed_Time := Current_Time - Start_Time; 
+        exit when Float(To_Duration(Elapsed_Time)) /600.0 > 10.0 ; 
+        
+           
+        Siguiente_Instante := Siguiente_Instante + Periodo;
+
+    end loop;
+    
+
+
+    Calefactor.Escribir(0.0);
+
+end Principal;
+
+--Periodo: constant Time_Span := To_Time_Span(Ts);
+--Siguiente_Instante: Time := Clock;
